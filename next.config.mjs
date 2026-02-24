7aea28e9cf49de097abaa3a7b9564dc6fd1a8604
Abhijeet Dangi <your-github-email>
2026-02-24 21:30:45 +0530

    fix dns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} /
+const nextConfig = {
+async rewrites() {
+return [
+{
+source: '/supabase-proxy/:path',
+destination: ${process.env.NEXT_PUBLIC_SUPABASE_URL}/:path*,
+},
+];
+},
+};
 
 export default nextConfig;
